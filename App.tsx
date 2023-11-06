76eee0dc2ce6fb6496e1a7df71e844ee1e046072
Wallace Neto <wallace.neto@outlook.com>
2023-11-05 23:13:10 -0300

    Aula 4

--- App.tsx
+++ App.tsx
@@ -1,47 +1,96 @@
-import React, {useState} from 'react';
+/* eslint-disable no-eval */
+import React, {Component} from 'react';
 import {SafeAreaView, View} from 'react-native';
 
 import styles from './src/global/styles';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 
-function App(): JSX.Element {
-  const [value, setValue] = useState('0');
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  currentIndex: 0,
+};
 
-  const addDigit = (n: string) => {
-    setValue(`${n}`);
-  };
-  const clearMemory = () => {
-    setValue('0');
-  };
-  const setOperation = (operation: string) => {
-    console.warn(operation);
+class App extends Component {
+  state = {...initialState};
+
+  addDigit = (n: string) => {
+    const clearDisplay =
+      (this.state.displayValue === '0' && n !== '.') || this.state.clearDisplay;
+
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
+      return;
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({displayValue, clearDisplay: false});
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[this.state.currentIndex] = newValue;
+      this.setState({values});
+    }
   };
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Display text={`${value}`} />
-      <View style={styles.buttons}>
-        <Button label={'AC'} triple onClick={clearMemory} />
-        <Button label={'/'} operation onClick={setOperation} />
-        <Button label={'7'} onClick={addDigit} />
-        <Button label={'8'} onClick={addDigit} />
-        <Button label={'9'} onClick={addDigit} />
-        <Button label={'*'} operation onClick={setOperation} />
-        <Button label={'4'} onClick={addDigit} />
-        <Button label={'5'} onClick={addDigit} />
-        <Button label={'6'} onClick={addDigit} />
-        <Button label={'-'} operation onClick={setOperation} />
-        <Button label={'1'} onClick={addDigit} />
-        <Button label={'2'} onClick={addDigit} />
-        <Button label={'3'} onClick={addDigit} />
-        <Button label={'+'} operation onClick={setOperation} />
-        <Button label={'0'} double onClick={addDigit} />
-        <Button label={'.'} onClick={addDigit} />
-        <Button label={'='} operation onClick={setOperation} />
-      </View>
-    </SafeAreaView>
-  );
+  clearMemory = () => {
+    this.setState(initialState);
+  };
+
+  setOperation = (operation: string) => {
+    if (this.state.currentIndex === 0) {
+      this.setState({operation, currentIndex: 1, clearDisplay: true});
+    } else {
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+      const equals = operation === '=';
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        // clearDisplay: !equals,
+        clearDisplay: true,
+        values,
+      });
+    }
+  };
+
+  render(): React.ReactNode {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Display text={`${this.state.displayValue}`} />
+        <View style={styles.buttons}>
+          <Button label={'AC'} triple onClick={this.clearMemory} />
+          <Button label={'/'} operation onClick={this.setOperation} />
+          <Button label={'7'} onClick={this.addDigit} />
+          <Button label={'8'} onClick={this.addDigit} />
+          <Button label={'9'} onClick={this.addDigit} />
+          <Button label={'*'} operation onClick={this.setOperation} />
+          <Button label={'4'} onClick={this.addDigit} />
+          <Button label={'5'} onClick={this.addDigit} />
+          <Button label={'6'} onClick={this.addDigit} />
+          <Button label={'-'} operation onClick={this.setOperation} />
+          <Button label={'1'} onClick={this.addDigit} />
+          <Button label={'2'} onClick={this.addDigit} />
+          <Button label={'3'} onClick={this.addDigit} />
+          <Button label={'+'} operation onClick={this.setOperation} />
+          <Button label={'0'} double onClick={this.addDigit} />
+          <Button label={'.'} onClick={this.addDigit} />
+          <Button label={'='} operation onClick={this.setOperation} />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 export default App;
